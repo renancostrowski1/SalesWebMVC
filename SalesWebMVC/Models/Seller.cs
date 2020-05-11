24ccd3eef40ac9ef36c99c43c8efc56c848e3ef5
Renan Ostrowski <renan_ostrowski@hotmail.com>
2020-05-11 10:46:48 -0300

    Validation

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -8,19 +8,32 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3,ErrorMessage = "{0} size should be between {1} and {2}")]
         public String Name { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage ="Enter valid Email")]
         [DataType(DataType.EmailAddress)]
         public String Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name="Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0,ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name="Salary base")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
         public double BaseSalary { get; set; }
+
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
 
         public Seller()
