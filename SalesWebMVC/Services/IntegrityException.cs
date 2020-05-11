6268cc8167f201567bf04b2aa03b2cdeb2550754
Renan Ostrowski <renan_ostrowski@hotmail.com>
2020-05-11 17:30:10 -0300

    Implementing simple search

---
+++ SalesWebMVC/Services/IntegrityException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMVC.Services
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(String message) : base(message)
+        {
+
+        }
+    }
+}
